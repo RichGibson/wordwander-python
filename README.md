792eae4aa4e9a995abbc936f769991d5a2acf49f
Rich Gibson <Rich.Gibson@gmail.com>
2015-11-12 19:52:35 -0800

    now with basic concordance

--- README.md
+++ README.md
@@ -3,3 +3,14 @@ Word Wander - Explore German vocabulary in a slightly innovative way.
 This is the code behind http://wordwander.com
 
 Prost!
+
+
+...
+notes...
+
+german extensions to nltk, which allows synonum browsing nad the like. good stuff.
+https://docs.google.com/document/d/1rdn0hOnJNcOBWEZgipdDfSyjJdnv_sinuAUSDSpiQns/edit?hl=en
+
+
+NLTK...
+pip install nltk
--- wordwander/views.py
+++ wordwander/views.py
@@ -1,3 +1,7 @@
+##### -*- coding: utf-8 -*-
+
+import codecs
+import operator
 
 from django.conf import settings
 from django.shortcuts import render, render_to_response
@@ -7,33 +11,79 @@ import re
 import sys
 from django.db import connection, transaction
 from wordwander.models import Word
-
+import pdb
 
 def home(request):
+    textblock=request.GET.get('textblock','')
+    #print >>sys.stderr, textblock
+    print >>sys.stderr, "start home"    
     prefix = request.GET.get('prefix','')
     root = request.GET.get('root','')
     suffix = request.GET.get('suffix','')
     top10k = request.GET.get('top10k',0)
+    #pdb.set_trace()
 
-     
+    concordance = ''
+    words = []
+    if len(textblock) > 0:
+        concordance = make_concordance(textblock)
+        
     (query, cnt_query) = make_query(prefix, root, suffix, top10k)
+
+    
     print >>sys.stderr, "query:%r" % query
     if len(query)>0:
-        words = Word.objects.raw(query)
-        
-
+        words = Word.objects.raw(query)       
         print >>sys.stderr, 'query run!'
-        for w in words:
-            print >>sys.stderr, "w:%r" % w
-        print >>sys.stderr, "done printing results"
     else:
         print >>sys.stderr, 'query not run'
-        words = []
         
-    return render_to_response('home.html', {'prefix':prefix, 'root':root, 'suffix':suffix, 'query':query, 'cnt_query':cnt_query, 'words':words })
+    #pdb.set_trace()   
+    return render_to_response('home.html', {'prefix':prefix, 'root':root, 'suffix':suffix, 'query':query, 'cnt_query':cnt_query, 'words':words, 'concordance':concordance })
 
+def add_words(line, words):
+    """ add the words in line to the dict words """ 
+    for word in line.split():
+        #pdb.set_trace()
+        if word in words.keys():
+            words[word] = words[word]+1
+        else:
+            words[word] = 1
+
+
+
+def clean_text(line):
+    #pdb.set_trace()   
+    if re.search('[a-zA-Z]', line):
+        line = line
+    else:
+        line = ''
+    #pdb.set_trace()
+    line=re.sub("\W"," ",line)
+    line=re.sub("[\.\"\-!\'\\\)\(,\-)?]", "",line)
+    #line=re.sub('\.','',line)
+    #line=re.sub('"','',line)
+    #line=re.sub("!",'',line)
+    #line=re.sub("'",'',line)
+    #line=re.sub('\(','',line)
+    #line=re.sub('\)','',line)
+    #line=re.sub(',','',line)
+    #line=re.sub('\?','',line)
+    #line=re.sub('-','',line)
+    line=re.sub('”','',line)
+    line=re.sub('“','',line)
+    line=re.sub('„','',line)
+    line=re.sub('…','',line)
+    line= line.lower()
+
+    return line    
     
-    
+def make_concordance(textblock):
+    words = {}
+    textblock = clean_text(textblock)
+    add_words(textblock, words)
+    return words
+
 def make_query(prefix,root,suffix,top10k=0):
 
     top10k_clause = ''
